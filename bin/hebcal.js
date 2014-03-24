22f8362e9fa5f2f9fe9775fe0f722b07091eda04
Scimonster <tehalmightyscimonster@gmail.com>
2014-03-24 15:43:03 +0200

    CLI: document acceptable dates.

--- bin/hebcal.js
+++ bin/hebcal.js
@@ -18,7 +18,7 @@ var helpString = [
 		["-c", "--candles", "Print candle-lighting and havdalah times"],
 		["",   "--candleLighting=NUM", "Set the time for candle-lighting to NUM minutes before sunset"],
 		["",   "--city=CITY", "Set the city to the given CITY"],
-		["",   "--day=DATE", "Print times for the given DAY"],
+		["",   "--day=DATE", "Print times for the given DATE. See hebcaljs -h dates for acceptable parameters"],
 		["",   "--dafyomi", "Print Daf Yomi for each day"],
 		["-g", "--greg", "Print the Gregorian date for each day"],
 		["",   "--hallel", "Print what Hallel is said on each date"],
@@ -41,7 +41,7 @@ var helpString = [
 	"Required parameters to long arguments are also required to the short forms.",
 	"The -O and -t flags cannot be used at the same time. Only one of -w -m -y can be used at a time. -T is ignored with -m or -y.",
 	"Table formatting is not guaranteed to be good; in fact it's nearly guaranteed to be messed up at least twice.",
-	"Help subjects: times, cities, warranty, license. Warning: license prints the ENTIRE text of the GPL3; it is very long.",
+	"Help subjects: times, dates, cities, warranty, license. Warning: license prints the ENTIRE text of the GPL3; it is very long.",
 	"For information using Hebcal programatically, see https://github.com/hebcal/hebcal and https://github.com/hebcal/hebcal-js"
 ];
 
@@ -98,6 +98,18 @@ var opts = {
 					"Time names are case insensitive."
 				]));
 				break;
+			case 'dates':
+				console.log(
+					"Some examples of acceptable date syntax.\n" +
+					"Anything with a space must be surrounded in quotes.\n\n" +
+					"tomorrow\n" +
+					"yesterday\n" +
+					"14 Adar\n" +
+					"15 Nisan 5773\n" +
+					"א' תשרי\n" +
+					"65 Elul 5770"
+				);
+				break;
 			case 'cities':
 				var cities = [], length = 5;
 				Hebcal.cities.listCities().forEach(function(c,i){
@@ -116,7 +128,16 @@ var opts = {
 				break;
 			case 'warranty':
 				console.log(
-					"This program is free software: you can redistribute it and/or modify\nit under the terms of the GNU General Public License as published by\nthe Free Software Foundation, either version 3 of the License, or\n(at your option) any later version.\n\nThis program is distributed in the hope that it will be useful,\nbut WITHOUT ANY WARRANTY; without even the implied warranty of\nMERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\nGNU General Public License for more details.\n\nYou should have received a copy of the GNU General Public License\nalong with this program. If not, see <http://www.gnu.org/licenses/>."
+					"This program is free software: you can redistribute it and/or modify\n" +
+					"it under the terms of the GNU General Public License as published by\n" +
+					"the Free Software Foundation, either version 3 of the License, or\n" +
+					"(at your option) any later version.\n\n" +
+					"This program is distributed in the hope that it will be useful,\n" +
+					"but WITHOUT ANY WARRANTY; without even the implied warranty of\n" +
+					"MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n" +
+					"GNU General Public License for more details.\n\n" +
+					"You should have received a copy of the GNU General Public License\n" +
+					"along with this program. If not, see <http://www.gnu.org/licenses/>."
 				);
 				break;
 			case 'license':
@@ -128,7 +149,7 @@ var opts = {
 		process.kill();
 	},
 	v: function(){
-		console.log('Hebcal JS 2.1');
+		console.log('Hebcal JS 2.1.1');
 		process.kill();
 	},
 	q: function(){opts.quiet = true}
