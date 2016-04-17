71330889bffd6c20fab18ec89bb129f57768f53b
Scimonster <tehalmightyscimonster@gmail.com>
2016-04-17 14:32:00 +0300

    Fix #16

--- hdate.js
+++ hdate.js
@@ -178,7 +178,8 @@ prototype[getMonth] = function() {
 };
 
 prototype.getTishreiMonth = function() {
-	return (this[getMonth]() + MONTH_CNT(this[getFullYear]()) - 6) % MONTH_CNT(this[getFullYear]());
+	var nummonths = MONTH_CNT(this[getFullYear]());
+	return (this[getMonth]() + nummonths - 6) % nummonths || nummonths;
 };
 
 prototype.daysInMonth = function() {
@@ -221,7 +222,7 @@ prototype.setDate = function(date) {
    Gregorian date Sunday, December 31, 1 BC. */
 function hebrew2abs(d) {
 	var m, tempabs = d[getDate](), year = d[getFullYear]();
-	
+
 	if (d[getMonth]() < TISHREI) {
 		for (m = TISHREI; m <= MONTH_CNT(year); m++) {
 			tempabs += daysInMonth(m, year);
@@ -235,7 +236,7 @@ function hebrew2abs(d) {
 			tempabs += daysInMonth(m, year);
 		}
 	}
-	
+
 	return c.hebElapsedDays(year) - 1373429 + tempabs;
 }
 
@@ -248,10 +249,10 @@ function abs2hebrew(d) {
 	if (d >= 10555144) {
 		throw new RangeError("parameter to abs2hebrew " + d + " out of range");
 	}
-	
+
 	gregdate = greg.abs2greg(d);
 	hebdate = new HDate(1, TISHREI, (year = 3760 + gregdate[getFullYear]()));
-	
+
 	while (d >= hebrew2abs(hebdate.setFullYear(year + 1))) {
 		year++;
 	}
